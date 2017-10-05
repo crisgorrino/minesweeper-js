a05a9a2261f8bee5fd27a14c2f5da9c1254ce7dc
Cristian Gorrino <cristiangorrino@Eulalio-Lopez.local>
2017-10-04 17:03:27 -0700

    Refactor to simplify

---
+++ src/minesweeper.js
@@ -0,0 +1,12 @@
+//declare variables to create walls
+const blankLine = '  |   |  ';
+//guessline variable
+const guessLine = '1 |   |  ';
+//bombline
+const bombLine = '  | B |  ';
+
+//log blankLine to console
+console.log(`This is what a board with a guess and a bomb on it would look like:`);
+console.log(`${guessLine}`);
+console.log(`${bombLine}`);
+console.log(`${blankLine}`);
